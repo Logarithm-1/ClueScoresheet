398a2f99cac3de47ec0b3d184ffca88c60bee46b
Logan Richards <sdrahcir.log@me.com>
2021-12-26 07:41:05 -0700

    Initial Commit

---
+++ Clue/ClueApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClueApp.swift
+//  Clue
+//
+//  Created by Logan Richards on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct ClueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
